aeec7f5d52cf3d5f8c7df37d2782e918ffbc0403
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-13 12:19:44 +0000

    Hide and Show 'add task'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
       { 
         id: 1,
@@ -25,7 +26,7 @@ function App() {
       }
     ]
   )
-
+    
   // Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) +1
@@ -49,8 +50,8 @@ function App() {
 
   return (
     <div className="container">
-      < Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
