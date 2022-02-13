75dea1f4fc44d27a78743f898de5456e0e099eb0
Tomas Garcia <tomasgarciadev@gmail.com>
2022-02-13 19:58:42 +0000

    Fetching data from local server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-      { 
-        id: 1,
-        text: "Doctor Appointment",
-        day: "Feb 5th at 2:30 pm",
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: "Meeting at School",
-        day: "Feb 6th at 1:30 pm",
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: "Food Shopping",
-        day: "Feb 5th at 2:30 pm",
-        reminder: false,
-      }
-    ]
-  )
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
     
   // Add Task
   const addTask = (task) => {
